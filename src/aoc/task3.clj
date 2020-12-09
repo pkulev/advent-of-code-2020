b7394d479d2f4e8243b5692a67e77c46a740614a
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-12-10 00:16:16 +0300

    Solve task3*
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- src/aoc/task3.clj
+++ src/aoc/task3.clj
@@ -8,32 +8,40 @@
 
 (defn count-trees
   "Recursively count trees."
-  [input idx acc]
-  (let [line (first input)
-        rest-input (next input)
-        next-idx (if line (mod (+ idx 3) (count line)) 0)
-        num-trees (if (= (get line idx) \#) (+ acc 1) acc)]
-    (if (nil? line)
-      num-trees
-      (do
-        (debug-line line idx)
-        (recur rest-input next-idx num-trees)))))
+  [input right down]
+  (defn count-trees%
+   "Recursive inner function."
+   [input idx acc]
+   (let [line (first input)
+         rest-input (drop down input)
+         next-idx (if line (mod (+ idx right) (count line)) 0)
+         num-trees (if (= (get line idx) \#) (+ acc 1) acc)]
+     (if (nil? line)
+       num-trees
+       (do
+         (debug-line line idx)
+         (recur rest-input next-idx num-trees)))))
+
+  (count-trees% input 0 0))
 
 (defn solve-task
   "Solve task3."
   [input]
-  (count-trees input 0 0))
+  (count-trees input 3 1))
 
 (defn solve-*
   "Solve task3*."
   [input]
-  (count input))
+  (let [args (for [[right down] [[1 1] [3 1] [5 1] [7 1] [1 2]]]
+               [input right down])]
+    (reduce * (map (partial apply count-trees) args))))
 
 (defn solve
   "Solve whole task3."
   []
   (let [input (aoc.core/read-resource "task3.txt")]
-    (solve-task input)))
+    (solve-task input)
+    (solve-* input)))
 
 
 
